6f0712d9ff0af01eb4641178274d0f350e3e7dfe
Jeron Tre Perey <jeronperey@gmail.com>
2026-04-09 13:49:57 -0700

    Step 5: Add GET /users/:id route with 404 handling

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,6 +35,9 @@ const users = {
   ]
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
@@ -43,6 +46,16 @@ const findUserByName = (name) => {
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.get("/users", (req, res) => {
